a66d246580cf76386d0c7f4d0b2432b1c79ea6f8
David Zuber <zuber.david@gmx.de>
2015-06-27 21:24:27 +0200

    Initial commit

---
+++ config/urls.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name="home"),
+    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
+
+    # Django Admin
+    url(r'^admin/', include(admin.site.urls)),
+
+    # User management
+    url('^login/', auth_views.login, name='login'),
+    url('^logout/', auth_views.logout, name='logout'),
+    url(r'^users/', include("djangotest.users.urls", namespace="users")),
+
+    # Rest
+    url(r'^api/', include('djangotest.users.api.urls')),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
+    # Your stuff: custom urls includes go here
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        url(r'^400/$', 'django.views.defaults.bad_request'),
+        url(r'^403/$', 'django.views.defaults.permission_denied'),
+        url(r'^404/$', 'django.views.defaults.page_not_found'),
+        url(r'^500/$', 'django.views.defaults.server_error'),
+    ]
---
+++ djangotest/users/views.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView, RedirectView, UpdateView
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, viewsets
+from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
+
+from braces.views import LoginRequiredMixin
+
+from .forms import UserForm
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    # These next two lines tell the view to index lookups by username
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+
+class UserRedirectView(LoginRequiredMixin, RedirectView):
+    permanent = False
+
+    def get_redirect_url(self):
+        return reverse("users:detail",
+                       kwargs={"username": self.request.user.username})
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+
+    form_class = UserForm
+
+    # we already imported User in the view code above, remember?
+    model = User
+
+    # send the user back to their own page after a successful update
+    def get_success_url(self):
+        return reverse("users:detail",
+                       kwargs={"username": self.request.user.username})
+
+    def get_object(self):
+        # Only get the User record for the user making the request
+        return User.objects.get(username=self.request.user.username)
+
+
+class UserListView(LoginRequiredMixin, ListView):
+    model = User
+    # These next two lines tell the view to index lookups by username
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
