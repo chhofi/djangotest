8e63e136deb5ca8156b1bdd60410c6578b574100
David Zuber <zuber.david@gmx.de>
2015-07-07 14:17:53 +0200

    Turn on debug for now

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,6 +10,9 @@ from __future__ import absolute_import, unicode_literals
 
 from .common import *  # noqa
 
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
 # SECRET CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
