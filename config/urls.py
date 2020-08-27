63e36179a0b7de821111afc17ba52d0dcac7ddb6
Christian Hofmann <christian.hofi@gmail.com>
2020-08-27 14:46:13 +0200

    change admin.site.url syntax

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
     # Django Admin
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 
     # User management
     url('^login/', auth_views.login, name='login'),
