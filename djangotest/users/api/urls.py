d34a2dae5ee96e82a2875d72a0b39a46690b1635
Christian Hofmann <christian.hofi@gmail.com>
2020-08-27 15:42:11 +0200

    delete patterns import as it has been removed

--- djangotest/users/api/urls.py
+++ djangotest/users/api/urls.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url
 from rest_framework import routers
 
 from ..views import UserViewSet
