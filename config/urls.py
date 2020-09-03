ef947ffd31f814bc8a8ef1223c21bb2b60d694a6
Christian Hofmann <christian.hofi@gmail.com>
2020-09-03 09:25:34 +0200

    change syntax in config

--- config/urls.py
+++ config/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     path(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider'))
 
     # Your stuff: custom urls includes go here
+]
 
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 #if settings.DEBUG:
