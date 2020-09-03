d6e9e8ee7b73b403cea29eb4b333a55fae56b864
Christian Hofmann <christian.hofi@gmail.com>
2020-09-03 10:27:31 +0200

    test config change

--- config/urls.py
+++ config/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
 	"about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
     # Django Admin, use {% url 'admin:index' %}
-    path(settings.ADMIN_URL, admin.site.urls),
+    path(r'^admin/', admin.site.urls),
     # User management
     path("users/", include("my_awesome_project.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
